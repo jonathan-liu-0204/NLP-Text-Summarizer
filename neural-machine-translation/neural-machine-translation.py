3781baa2ecd7c7c91770bcd3cc1d959da4c4636a
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-21 19:49:33 +0800

    setup the field, vocabulary, basic model assets

--- neural-machine-translation/neural-machine-translation.py
+++ neural-machine-translation/neural-machine-translation.py
@@ -41,3 +41,27 @@ def tokenize_en(text):
     Tokenizes English text from a string into a list of strings
     """
     return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+# the fields
+SRC = Field(tokenize = tokenize_de, init_token = '<sos>', eos_token = '<eos>', lower = True)
+TRG = Field(tokenize = tokenize_en, init_token = '<sos>', eos_token = '<eos>', lower = True)
+
+
+# load all the data
+train_data, valid_data, test_data = Multi30k.splits(exts = ('.de', '.en'), fields = (SRC, TRG))
+
+
+# build the vocabulary
+SRC.build_vocab(train_data, min_freq = 2)
+TRG.build_vocab(train_data, min_freq = 2)
+
+
+# cuda or cpu
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Create the iterators.
+BATCH_SIZE = 128
+
+train_iterator, valid_iterator, test_iterator = BucketIterator.splits((train_data, valid_data, test_data), batch_size = BATCH_SIZE, device = device)
+
