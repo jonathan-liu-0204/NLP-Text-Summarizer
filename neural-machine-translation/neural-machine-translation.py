f8f40429d614d3db6e5a71f9076e5b06b61b4f17
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-21 19:39:05 +0800

    tokenizer & basic imports

---
+++ neural-machine-translation/neural-machine-translation.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from torchtext.legacy.datasets import Multi30k
+from torchtext.legacy.data import Field, BucketIterator
+
+import spacy
+import numpy as np
+
+import random
+import math
+import time
+
+
+#Set the random seeds for reproducability.
+SEED = 1234
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+
+#Load the German and English spaCy models.
+spacy_de = spacy.load('de_core_news_sm')
+spacy_en = spacy.load('en_core_web_sm')
+
+
+#create the tokenizers.
+def tokenize_de(text):
+    """
+    Tokenizes German text from a string into a list of strings
+    """
+    return [tok.text for tok in spacy_de.tokenizer(text)]
+
+def tokenize_en(text):
+    """
+    Tokenizes English text from a string into a list of strings
+    """
+    return [tok.text for tok in spacy_en.tokenizer(text)]
